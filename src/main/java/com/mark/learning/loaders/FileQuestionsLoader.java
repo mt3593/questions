ec127a3c60d28f9798e7f41bf5b0b2c05a2c036a
Mark Tinsley <mark.tinsley@mixrad.io>
2016-02-21 15:06:33 +0000

    added notes on java concurrency

--- src/main/java/com/mark/learning/loaders/FileQuestionsLoader.java
+++ src/main/java/com/mark/learning/loaders/FileQuestionsLoader.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 public class FileQuestionsLoader implements QuestionLoader {
@@ -37,7 +38,7 @@ public class FileQuestionsLoader implements QuestionLoader {
                 }
             }
             return questions;
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
